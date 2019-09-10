aac8296d1646ed446ce00fd0793443d063ad98d7
lauragift21 <laurandidi21@gmail.com>
2019-09-10 15:24:53 +0100

    gst

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
 import 'dotenv/config';
 
+const port = (process.env.PORT || 8080);
+
+// initialize express
 const app = express();
 
 app.use(cors());
 
+// Logging middleware to console
+app.use(logger('dev'));
+
+// Parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Population Management System API');
+  res.status(200).send({message: 'Welcome to Population Management System API'});
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`app listening on port ${process.env.PORT}`),
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'You\'ve hit a wrong endpoint, this endpoint does not exist'});
+});
+
+app.listen(port, () =>
+  console.log(`Welcome to Population Management Application, app listening on port ${port}`),
 );
+
+export default app;
